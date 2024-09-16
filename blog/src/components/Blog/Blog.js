18e5338b35f5b9744cd693d3f5a121a44a11e0be
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2024-09-16 13:48:53 +0300

    .

--- blog/src/components/Blog/Blog.js
+++ blog/src/components/Blog/Blog.js
@@ -17,40 +17,26 @@ function Blog() {
   }, []);
 
   return (
-    
     <div>
-      {data ? (
-        <div>
-          <h1>{data.title}</h1>
-          <h2>{data.author}</h2>
-          <hr />
-          <div>
-            {data.map((post, index) => (
-              <div key={index}>
-                {post.image && (
-        <img className="mb-5 
-        h-[400px] 
-        w-full 
-        rounded-xl 
-        bg-no-repeat 
-        object-cover 
-        object-center 
-        transition-transform 
-        duration-200 
-        ease-out 
-        hover:scale-[1.02]" 
-        src={post.image} 
-        alt={post.title}  />
-      )}
-                <h3>{post.title}</h3>
-                <p>{post.content}</p>
-              </div>
-            ))}
+      {data && data.map((post, index) => (
+        <div key={index}>
+          {post.image && (
+            <div className="max-w-4xl mx-auto p-5">
+            <img 
+              src={post.image} 
+              alt="Earth from Space"
+              className="w-full h-auto rounded-lg shadow-lg"
+            />
+            <h1 className="text-3xl font-bold mt-4 mb-2">{post.title}</h1>
+            <p className="text-gray-600 text-sm">
+              by <span className="font-semibold">{post.author}</span> on {post.date}
+            </p>
+            <hr className="my-4" />
+            <p className="text-gray-800 text-base leading-relaxed">{post.body}</p>
           </div>
+          )}
         </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+      ))}
     </div>
   );
 }
