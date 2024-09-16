f98f874bdcd8ed4f012fe3f55f9c956118afa6d9
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2024-09-16 12:04:26 +0300

    add image

--- blog/src/components/Blog/Blog.js
+++ blog/src/components/Blog/Blog.js
@@ -13,11 +13,44 @@ function Blog() {
       })
       .then(json => setData(json))
       .catch(error => console.error('Error fetching data:', error));
+      console.log(data);
   }, []);
 
   return (
+    
     <div>
-      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Loading...</p>}
+      {data ? (
+        <div>
+          <h1>{data.title}</h1>
+          <h2>{data.author}</h2>
+          <hr />
+          <div>
+            {data.map((post, index) => (
+              <div key={index}>
+                {post.image && (
+        <img className="mb-5 
+        h-[400px] 
+        w-full 
+        rounded-xl 
+        bg-no-repeat 
+        object-cover 
+        object-center 
+        transition-transform 
+        duration-200 
+        ease-out 
+        hover:scale-[1.02]" 
+        src={post.image} 
+        alt={post.title}  />
+      )}
+                <h3>{post.title}</h3>
+                <p>{post.content}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
