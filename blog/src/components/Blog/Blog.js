8359daeed097dd47367d2a060279bbb3d2e72cc3
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2024-09-17 09:47:57 +0300

    added article

--- blog/src/components/Blog/Blog.js
+++ blog/src/components/Blog/Blog.js
@@ -41,7 +41,7 @@ function Blog() {
     <body>
     <header className='sticky top-0 z-10 mx-auto bg-white/75 backdrop-blur-lg dark:bg-zinc-950/75'>
       <div className='mx-auto flex w-full max-w-3xl flex-col items-center justify-between px-4 py-4 md:flex-row lg:px-0'>
-      <h1 className='italic text-3xl font-bold underline decoration-cyan-200 hover:decoration-blue-400'>Our Blue Marble</h1>
+      <h1 className='italic text-blue-400 text-3xl font-bold underline decoration-blue-50 hover:decoration-blue-50'>Our Blue Marble</h1>
       <span className="relative hidden text-lg tracking-wide text-zinc-500 dark:text-zinc-200 md:flex">Thoughts and images from our amazing planet.</span>
     </div>
     </header>
@@ -64,7 +64,18 @@ function Blog() {
             </button>
             <div className="flex items-center space-x-2 text-start text-sm m-4">
             <img src={post.authorImage} alt="Author" className="h-10 w-10 rounded-full" />
-            <span>by <span className="font-semibold text-cyan-200">{post.author}</span> on {new Date(post.date).toLocaleDateString()}</span>
+            <span>by <span className="font-semibold text-cyan-200">{post.author}</span> on {post.date}</span>
+          </div>
+          <div>
+            <hr className="my-4" />
+            {
+              post.article.map((article, index) => (
+                <div key={index}>
+                  <h2 className="text-2xl font-bold mt-4 mb-2">{article.title}</h2> 
+                </div>
+              ))
+              
+            }
           </div>
           </div>
           )}
